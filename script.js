83e365bf589196f5c0e2a7a7efb678c21988b400
Ankur0942 <95076740+Ankur0942@users.noreply.github.com>
2026-02-24 10:51:13 +0530

    Structure for it's Console game

---
+++ script.js
@@ -0,0 +1,34 @@
+const board = (() => {
+    const game = [];
+
+})()
+
+const createPlayer = () => {
+    const playerX = ;
+    const playerO = ;
+};
+
+const gameController = (() => {
+
+    const playRound = () => { };
+
+    const checkWinner = () => {
+    const winMoves = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+};
+
+    const currentPlayer = () => { };
+
+    const switchPlayer = () => { };
+
+    const checkIfEmpty = () => { };
+})()
+
